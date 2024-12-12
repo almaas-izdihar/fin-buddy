3049c0d669be73f1372cb297d2669e9a67ed4a8d
okamyz <acidrainer257@gmail.com>
2024-12-12 22:00:28 +0700

    Progress Front-End

---
+++ pages/404.php
@@ -0,0 +1,4 @@
+<div class="container">
+    <h1 class="mt-5 " style="color:tomato; font-weight: 900;">ERROR 404</h1>
+    <button class="btn btn-danger" type="button" onclick="window.location.href='?page=home'">KEMBALI</button>
+</div>
